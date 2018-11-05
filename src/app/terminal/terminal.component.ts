7616029fbdf4213cdee593b0bba9cb1a79952459
ofirf <ofirf@sentinel.com>
2018-11-05 13:57:39 +0200

    added namespace

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Terminal } from 'xterm';
+import { TerminalSocket } from '../socket/terminal-socket';
+import * as fit from 'xterm/lib/addons/fit/fit';
 
 @Component({
   selector: 'app-terminal',
@@ -8,16 +10,27 @@ import { Terminal } from 'xterm';
 })
 export class TerminalComponent implements OnInit {
   @ViewChild('terminal') terminalElement;
+  private terminalSocket = TerminalSocket.instance;
+
 
   constructor() {
   }
 
   ngOnInit() {
 
+    Terminal.applyAddon(fit);
+    const term: any = new Terminal();
 
-    const term = new Terminal();
-
+    const roomName = 'terminal-1-message';
     term.open(this.terminalElement.nativeElement);
+    term.fit();
+    term.on('data', (data) => {
+      this.terminalSocket.socket.emit(roomName, data);
+    });
+
+    this.terminalSocket.socket.on(roomName, (data) => {
+      term.write(data.text);
+    });
 
   }
 
