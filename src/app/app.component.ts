52688319544307c82503db3c20dac2c4e2b092d7
ofirf <ofirf@sentinel.com>
2018-11-05 17:23:11 +0200

    added namespace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Subject } from 'rxjs';
-import { TerminalSocket } from './socket/terminal-socket';
-import { DashboardSocket } from './socket/dashboard-socket';
-
 
 @Component({
   selector: 'app-root',
@@ -10,61 +6,6 @@ import { DashboardSocket } from './socket/dashboard-socket';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  dashboard;
-  terminalRoom1;
-  terminalRoom2;
-
-  private dashboardSocket = DashboardSocket.instance;
-  private terminalSocket = TerminalSocket.instance;
-
-
-  input$ = new Subject();
-  input2$ = new Subject();
-  input3$ = new Subject();
-
   constructor() {
-    this.dashboardTest();
-    this.terminalRoom1Test();
-    this.terminalRoom2Test();
   }
-
-  dashboardTest(): void {
-    const roomName = 'dashboard-message';
-    this.dashboardSocket.socket.on(roomName, (data) => {
-      console.log('data: ', data);
-      this.dashboard = data;
-      console.log('dashboard: ', this.dashboard);
-    });
-
-    this.input$.subscribe((data) => {
-      this.dashboardSocket.socket.emit(roomName, data);
-    });
-  }
-
-  terminalRoom1Test(): void {
-    const roomName = 'terminal-1-message';
-    this.terminalSocket.socket.on(roomName, (data) => {
-      console.log('data: ', data);
-      this.terminalRoom1 = data;
-      console.log('terminal: ', this.terminalRoom1);
-    });
-
-    this.input2$.subscribe((data) => {
-      this.terminalSocket.socket.emit(roomName, data);
-    });
-  }
-
-  terminalRoom2Test(): void {
-    const roomName = 'terminal-2-message';
-    this.terminalSocket.socket.on(roomName, (data) => {
-      console.log('data: ', data);
-      this.terminalRoom2 = data;
-      console.log('terminal-2: ', this.terminalRoom1);
-    });
-
-    this.input3$.subscribe((data) => {
-      this.terminalSocket.socket.emit(roomName, data);
-    });
-  }
-
 }
