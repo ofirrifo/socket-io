81198d15ca6166ebe643b98181695e0f491f98e1
ofirf <ofirf@sentinel.com>
2018-11-08 12:56:50 +0200

    added namespace

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
 import { ITerminalOptions, Terminal } from 'xterm';
 import { TerminalSocket } from '../socket/terminal-socket';
 import * as fit from 'xterm/lib/addons/fit/fit';
@@ -29,14 +29,17 @@ export class TerminalComponent implements OnInit, AfterViewInit {
   @Input() titleText: string;
 
   @ViewChild('terminal') terminalElement;
-  @ViewChild('terminalOutput') terminalOutputElement;
-  private terminalSocket = TerminalSocket.instance;
-  startTime;
-  endTime;
-  et;
-  prompt = '-> Dev';
 
-  isOnline = false;
+  term: any;
+  keyToMethod = {
+    '13': this.onEnter.bind(this),
+    '8': this.onBackspace.bind(this),
+    '37': this.onLeftArrow.bind(this),
+    '39': this.onRightArrow.bind(this),
+  };
+  prompt = '=> Dev ';
+  text = '';
+  cursorPosition = 0;
 
 
   constructor(private cd: ChangeDetectorRef) {
@@ -48,28 +51,85 @@ export class TerminalComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.terminal();
-    this.terminalSocket.emit(this.roomName, '±');
   }
 
   terminal() {
     Terminal.applyAddon(fit);
     const terminalOptions = this.withOutput ? TERMINAL_OPTIONS_DISABLE : TERMINAL_OPTIONS;
-    const term: any = new Terminal(terminalOptions);
+    this.term = new Terminal(terminalOptions);
 
-    term.on('key', (data, event) => {
-      term.write(data);
-      this.et = data;
+    this.term.on('key', (data, event) => {
+      // console.table([data, event]);
+      console.log(event.keyCode);
+      if (this.keyToMethod.hasOwnProperty(event.keyCode)) {
+        this.keyToMethod[event.keyCode](data, event);
+      } else {
+        this.onWrite(data);
+      }
     });
 
-    term.open(this.terminalElement.nativeElement);
+    this.term.open(this.terminalElement.nativeElement);
 
-    term.fit();
+    this.term.fit();
 
   }
 
-  send(textElement): void {
-    this.terminalSocket.emit(this.roomName, textElement.value + '\r\n');
-    textElement.value = '';
+  onEnter(): void {
+    this.term.writeln('');
+    if (this.text) {
+      this.term.write(this.text);
+      this.term.writeln('');
+    }
+    this.term.write(this.prompt);
+    this.text = '';
+    this.updateCursorPosition('reset');
   }
 
+  onBackspace() {
+    if (this.cursorPosition > 0) {
+      this.term.write('\b \b');
+      this.updateCursorPosition('remove');
+    }
+  }
+
+  onLeftArrow(data) {
+    this.updateCursorPosition('remove');
+    if (this.cursorPosition > -1) {
+      this.term.write(data);
+    }
+  }
+
+  onRightArrow(data) {
+    this.updateCursorPosition('add');
+    if (this.cursorPosition < this.text.length) {
+      this.term.write(data);
+    }
+  }
+
+  /**
+   * write the enter text to terminal
+   */
+  onWrite(data) {
+    this.term.write(data);
+    this.text += data;
+    this.updateCursorPosition('add');
+  }
+
+  updateCursorPosition(action: string): void {
+    if (action === 'add') {
+      if (this.cursorPosition < this.text.length) {
+        this.cursorPosition++;
+      }
+    } else if (action === 'remove') {
+      this.cursorPosition--;
+    } else if (action === 'reset') {
+      this.cursorPosition = 0;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.term.resize();
+    this.term.fit();
+  }
 }
