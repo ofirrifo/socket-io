42135dcb1d530684581dac529f246fc1510555b7
ofirf <ofirf@sentinel.com>
2018-11-08 10:19:47 +0200

    added namespace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
-import { LedComponent } from './led/led.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TerminalComponent,
-    LedComponent
   ],
   imports: [
     BrowserModule
@@ -17,4 +15,5 @@ import { LedComponent } from './led/led.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
